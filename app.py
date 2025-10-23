23a689498b4e060eb1479d14769013ecc73c4b65
Sumit Kumar <sumit.kumar@opalina.in>
2025-10-23 18:24:53 +0530

    Update model loading and save improved model format

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 #Load the LSTM Model
-model=load_model('next_word_lstm.h5')
+# model=load_model('next_word_lstm.h5')
+model=load_model('next_word_lstm.keras')
 
 #3 Laod the tokenizer
 with open('tokenizer.pickle','rb') as handle:
